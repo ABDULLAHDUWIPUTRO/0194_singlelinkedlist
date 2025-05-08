2d0caa53f0fc39f34eede98c43be8b2cb590708a
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2025-05-08 09:59:34 +0700

    first commit

---
+++ FILE1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Node
+{
+    public:
+        int noMhs;
+        Node *next;
+};
+
+class List
+{
+    Node *START;
+
+    public:
+       List()
+       {
+        START = NULL;
+       }
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukan Nomor Mahasiswa: ";
+        cin >> nim;
+    }
