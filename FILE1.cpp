f30adf2ef185ab35a6e689477c4bd026203a2866
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2025-05-08 11:21:35 +0700

    Menambahkan fitur pencarian data mahasiswa dalam linked list" atau "Implementasi pencarian data dengan validasi dalam linked list".

--- FILE1.cpp
+++ FILE1.cpp
@@ -146,17 +146,35 @@ int main()
                 break;
             }
             cout << endl
-            << "\nMasukan no mahasiswa yang akan dihapus : ";
+                 << "\nMasukan no mahasiswa yang akan dihapus : ";
             cin >> nim;
             if (Mhs.delNode(nim) == false)
-            cout << endl
-            << "data tidak ditemukan" << endl;
+                cout << endl
+                     << "data tidak ditemukan" << endl;
             else
-            cout << endl
-                 << "data dengan nomor mahasiswa" << nim << " berhasil dihapus " << endl;
+                cout << endl
+                     << "data dengan nomor mahasiswa" << nim << " berhasil dihapus " << endl;
         }
         break;
         case '3':
         {
             Mhs.traverse();
         }
+        break;
+        case '4':
+        {
+            if (Mhs.listEmpty() == true)
+            {
+                cout << "\nList kosong\n";
+                break;
+            }
+            Node *previous, *current;
+            cout << endl
+                 << "masukan no mahasiswa yang dicari : ";
+            cin >> nim;
+            if (Mhs.Search(nim, &previous, &current) == false)
+                cout << endl
+                     << "data tidak dittemukann" << endl;
+            else
+            {
+            }
