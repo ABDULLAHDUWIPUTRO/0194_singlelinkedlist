c1a11a8475fc0f4f783e4d2cc50607b8e63dddc1
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2025-05-08 10:18:26 +0700

    membuat list empty

--- FILE1.cpp
+++ FILE1.cpp
@@ -47,4 +47,14 @@ while ((current != NULL) && (nim >= current->noMhs))
   if (nim == current->noMhs)
   {
     cout << "\nDuplikasi noMhs tidak diijinkan\n";
+    return;
   }
+  previous = current;
+  current = current->next;
+}
+
+nodebaru->next = current;
+previous->next = nodebaru;
+    }
+
+bool listEmpty()
